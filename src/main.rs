80ee1248409ce52cbda2f3a464f67059a68a90e2
tpmccallum <mistermac2008@gmail.com>
2020-08-01 11:08:45 +1000

    Remove byte array dependency and just use [..] natively to achieve the same thing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-// Dependencies
-use byte_array::ByteArray;
-
 // Bring into scope
 use rocksdb::DB;
 
@@ -12,18 +9,12 @@ let db_path = "/media/nvme/ssvm_database";
 // Open DB instance
 let db = DB::open_default(db_path).unwrap();
 
-// Create key and value
-let mut key = ByteArray::new();
-let mut value = ByteArray::new();
-key.write(&(1234 as u8));
-value.write(&(5678 as u8));
-
 
 // Store
-db.put(key, value).unwrap();
-println("Value of : {:?}, Stored to key: {:?}", value, key);
+db.put(&b"1234"[..], &b"5678"[..]).unwrap();
+println!("Value stored ... ");
 
 //Load
-let loaded_data = db.get(&key).unwrap().unwrap();
-println!("Value of : {:?}, Loaded from key: {:?}", value, key);
+let loaded_data = db.get(&b"1234"[..]).unwrap().unwrap();
+println!("Value of : {:?}, was successfully loaded.", loaded_data);
 }
