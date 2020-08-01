7e32c7deb63b90981a17da81aeb787fb843e9449
tpmccallum <mistermac2008@gmail.com>
2020-08-01 10:51:16 +1000

    structure with main so we can run this

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,26 @@ use byte_array::ByteArray;
 // Bring into scope
 use rocksdb::DB;
 
+
 fn main() {
-    // Set config
-    let db_path = "/media/nvme/ssvm_database";
+// Set config
+let db_path = "/media/nvme/ssvm_database";
 
-    // Open DB instance
-    let db = DB::open_default(db_path).unwrap();
+// Open DB instance
+let db = DB::open_default(db_path).unwrap();
 
-    // Create key and value
-    let mut key = ByteArray::new();
-    let mut value = ByteArray::new();
-    key.write((1234 as u8));
-    value.write((5678 as u8));
+// Create key and value
+let mut key = ByteArray::new();
+let mut value = ByteArray::new();
+key.write(1234 as u8);
+value.write(5678 as u8);
 
-    // Store
-    db.put(key, value).unwrap();
-    println("Value of : {:?}, Stored to key: {:?}", value, key);
 
-    //Load
-    let loaded_data = db.get(&key).unwrap().unwrap();
-    println("Value of : {:?}, Loaded from key: {:?}", value, key);
+// Store
+db.put(key, value).unwrap();
+println("Value of : {:?}, Stored to key: {:?}", value, key);
+
+//Load
+let loaded_data = db.get(&key).unwrap().unwrap();
+println!("Value of : {:?}, Loaded from key: {:?}", value, key);
 }
-
